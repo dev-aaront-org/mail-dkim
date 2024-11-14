f210e7bd41a3d817ba26b0a0f55bea70c6a51cd6
Aaron Thompson <dev@aaront.org>
2024-11-14 03:04:50 +0000

    Signer: Add a helper function for loading a private key file
    This keeps the key type determination logic in one place.

--- lib/Mail/DKIM/Signer.pm
+++ lib/Mail/DKIM/Signer.pm
@@ -196,13 +196,9 @@ sub init {
         $self->{'Algorithm'} = 'rsa-sha1';
     }
 
-    my $type = 'rsa'; # default
-    $type = 'ed25519' if ( $self->{'Algorithm'} =~ /^ed25519/ );
-
     if ( defined $self->{KeyFile} ) {
         $self->{Key} ||=
-          Mail::DKIM::PrivateKey->load( File => $self->{KeyFile},
-            Type => $type );
+          load_private_key( $self->{KeyFile}, $self->{Algorithm} );
     }
 
     unless ( $self->{'Method'} ) {
@@ -314,9 +310,6 @@ sub finish_body {
         # finished canonicalizing
         $algorithm->finish_body;
 
-        my $type = 'rsa'; # default
-        $type = 'ed25519' if ( $self->{'Algorithm'} =~ /^ed25519/ );
-
         # load the private key file if necessary
         my $signature = $algorithm->signature;
         my $key =
@@ -325,8 +318,7 @@ sub finish_body {
           || $self->{Key}
           || $self->{KeyFile};
         if ( defined($key) && !ref($key) ) {
-            $key = Mail::DKIM::PrivateKey->load( File => $key,
-                Type => $type );
+            $key = load_private_key( $key, $self->{Algorithm} );
         }
         $key
           or die "no key available to sign with\n";
@@ -341,6 +333,17 @@ sub finish_body {
         $self->{signature} = $signature;
         $self->{result}    = 'signed';
     }
+}
+
+# Load a private key file for the given algorithm.
+sub load_private_key {
+    my $key_file = shift;
+    my $algorithm = shift;
+
+    my $type = 'rsa'; # default
+    $type = 'ed25519' if ( $algorithm =~ /^ed25519/ );
+
+    return Mail::DKIM::PrivateKey->load( File => $key_file, Type => $type );
 }
 
 =head1 METHODS
